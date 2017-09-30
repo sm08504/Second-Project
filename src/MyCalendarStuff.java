213178a8a5a96082aa1861956cc310764caa9236
Serggio Montero <Serggio Montero@10.0.1.13>
2017-09-30 01:28:30 -0400

    New space

---
+++ src/MyCalendarStuff.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class MyCalendarStuff {
+
+	public static void main(String[] args) {
+	LocalDate current = LocalDate.now();
+	System.out.println("Today is " + current.getDayOfWeek() + " " + current.getDayOfMonth() + " " + current.getMonth());
+
+	
+	LocalDate startOfSemester = LocalDate.of(2017,  8, 14);
+	System.out.println("Semester started " + startOfSemester.getDayOfWeek() + " " + startOfSemester.getDayOfMonth() + " " + startOfSemester.getMonth());
+	
+	Period p = Period.between(startOfSemester,  current);
+	System.out.println("It is " + p.getMonths() + " months and" + p.getDays() + " days since semester started");
+	}
+
+}
